024cca36195bdb8dbbc2ca9734115eae2dba4fda
Jonattan Salcedo Alvarez <jssalcedo88@gmail.com>
2023-01-02 15:28:16 -0600

    Websocket and subscription server implementation.

--- client/src/graphql/hooks.js
+++ client/src/graphql/hooks.js
@@ -11,6 +11,11 @@ export function useAddMessage() {
         context: {
           headers: { 'Authorization': 'Bearer ' + getAccessToken() },
         },
+        update: (cache, { data: { message } }) => {
+          cache.updateQuery({ query: MESSAGES_QUERY }, (oldData) => {
+            return { messages: [...oldData.messages, message] };
+          });
+        },
       });
       return message;
     },
